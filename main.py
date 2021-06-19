dcc8ef24c8ae61fc0240ebdeab34a0136b6219c3
Juxhee <60679596+Juxhee@users.noreply.github.com>
2021-06-19 20:18:15 +0900

    Update main.py

--- main.py
+++ main.py
@@ -33,19 +33,6 @@ warnings.filterwarnings('ignore')
 
 
 def focal_loss(labels, logits, alpha, gamma):
-    """Compute the focal loss between `logits` and the ground truth `labels`.
-    Focal loss = -alpha_t * (1-pt)^gamma * log(pt)
-    where pt is the probability of being classified to the true class.
-    pt = p (if true class), otherwise pt = 1 - p. p = sigmoid(logit).
-    Args:
-      labels: A float tensor of size [batch, num_classes].
-      logits: A float tensor of size [batch, num_classes].
-      alpha: A float tensor of size [batch_size]
-        specifying per-example weight for balanced cross entropy.
-      gamma: A float scalar modulating loss from hard and easy examples.
-    Returns:
-      focal_loss: A float32 scalar representing normalized total loss.
-    """
     BCLoss = F.binary_cross_entropy_with_logits(input = logits, target = labels,reduction = "none")
 
     if gamma == 0.0:
